019997c6c831473af3f2517afb930cbbb136b1fc
dmytrobohdanov <dmytro.bohdanov@gmail.com>
2023-03-10 12:46:59 +0200

    Initial commit

---
+++ app/src/main/java/com/bohdanov/bootounter/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.bohdanov.bootounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bohdanov.bootounter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.activityMainBootText.text = "hello world"
+    }
+}
