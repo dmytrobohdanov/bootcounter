c2fcec6aa0d14c4fff39d722f225c96e62357997
dmytrobohdanov <dmytro.bohdanov@gmail.com>
2023-03-10 14:19:35 +0200

    added events saver

--- app/src/main/java/com/bohdanov/bootounter/app/booteventsreceiver/BootEventsReceiver.kt
+++ app/src/main/java/com/bohdanov/bootounter/app/booteventsreceiver/BootEventsReceiver.kt
@@ -3,14 +3,16 @@ package com.bohdanov.bootounter.app.booteventsreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.bohdanov.bootounter.data.BootDataRepository
 
 class BootEventsReceiver : BroadcastReceiver() {
+    lateinit var repository : BootDataRepository
     override fun onReceive(context: Context, intent: Intent) {
         val action = intent.action
 
         action?.let {
             if (it == Intent.ACTION_BOOT_COMPLETED) {
-                TODO("write event to repo")
+                repository.saveBootData(System.currentTimeMillis())
             }
         }
     }
